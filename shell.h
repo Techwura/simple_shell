f63f51212f29f93dec2a48fe835f469f1ff43b6b
Techwura <afiaserwaaamponsem@gmail.com>
2023-12-16 12:14:42 -0800

    simpleshellproj

--- shell.h
+++ shell.h
@@ -18,28 +18,30 @@
 #define BUF_FLUSH -1
 
 /* for command chaining */
-#define CMD_NORM        0
-#define CMD_OR                1
-#define CMD_AND                2
-#define CMD_CHAIN        3
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
 
 /* for convert_number() */
-#define CONVERT_LOWERCASE        1
-#define CONVERT_UNSIGNED        2
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
 
 /* 1 if using system getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
-#define HIST_FILE        ".simple_shell_history"
-#define HIST_MAX        4096
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
 
 extern char **environ;
+
+
 /**
- * struct liststr - the entry point.
- * Description - singly linked list.
- * @num: the number field.
- * @str: a string.
- * @next: points to the next node.
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
  */
 typedef struct liststr
 {
@@ -49,27 +51,26 @@ typedef struct liststr
 } list_t;
 
 /**
- * struct passinfo - the entry point.
- * Description - contains pseudo-arguements to pass into a function,
- * allowing uniform prototype for function pointer struct.
- * @arg: a string generated from getline containing arguem.ents
- * @argv:an array of strings generated from arg.
- * @path: a string path for the current command.
- * @argc: the argument count.
- * @line_count: the error count.
- * @err_num: the error code for exit()s.
- * @linecount_flag: if on count this line of input.
- * @fname: the program filename.
- * @env: linked list local copy of environ.
- * @environ: custom modified copy of environ from LL env.
- * @history: the history node.
- * @alias: the alias node.
- * @env_changed: on if environ was changed.
- * @status: the return status of the last exec'd command.
- * @cmd_buf: address of pointer to cmd_buf, on if chaining.
- * @cmd_buf_type: CMD_type ||, &&, ;.
- * @readfd: the fd from which to read line input.
- * @histcount: the history line number count.
+ * struct passinfo - contains pseudo-arguements to pass into a function,
+ * allowing uniform prototype for function pointer struct
+ * @arg: a string generated from getline containing arguements
+ * @argv:an array of strings generated from arg
+ * @path: a string path for the current command
+ * @argc: the argument count
+ * @line_count: the error count
+ * @err_num: the error code for exit()s
+ * @linecount_flag: if on count this line of input
+ * @fname: the program filename
+ * @env: linked list local copy of environ
+ * @environ: custom modified copy of environ from LL env
+ * @history: the history node
+ * @alias: the alias node
+ * @env_changed: on if environ was changed
+ * @status: the return status of the last exec'd command
+ * @cmd_buf: address of pointer to cmd_buf, on if chaining
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @readfd: the fd from which to read line input
+ * @histcount: the history line number count
  */
 typedef struct passinfo
 {
@@ -87,6 +88,7 @@ typedef struct passinfo
 	char **environ;
 	int env_changed;
 	int status;
+
 	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
 	int cmd_buf_type; /* CMD_type ||, &&, ; */
 	int readfd;
@@ -95,13 +97,12 @@ typedef struct passinfo
 
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-	0, 0, 0}
+		0, 0, 0}
 
 /**
- * struct builtin - the entry point.
- * Description - contains a builtin string and related function.
- * @type: the builtin command flag.
- * @func: the function.
+ * struct builtin - contains a builtin string and related function
+ * @type: the builtin command flag
+ * @func: the function
  */
 typedef struct builtin
 {
@@ -109,7 +110,8 @@ typedef struct builtin
 	int (*func)(info_t *);
 } builtin_table;
 
-/* toem_sh_loop.c */
+
+/* toem_shloop.c */
 int hsh(info_t *, char **);
 int find_builtin(info_t *);
 void find_cmd(info_t *);
@@ -155,7 +157,7 @@ char *_memset(char *, char, unsigned int);
 void ffree(char **);
 void *_realloc(void *, unsigned int, unsigned int);
 
-/* toem_mem.c */
+/* toem_memory.c */
 int bfree(void **);
 
 /* toem_atoi.c */
